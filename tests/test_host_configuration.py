36fe2f476ab6c65136fbbfd69adf70ea4d494f80
Chris Talkington <chris@talkingtontech.com>
2022-03-03 23:30:43 -0600

    Update test_host_configuration.py

--- tests/test_host_configuration.py
+++ tests/test_host_configuration.py
@@ -229,7 +229,7 @@ async def test_host_configuration_url_no_port() -> None:
 
     client = RadarrClient(
         host_configuration=PyArrHostConfiguration(
-            api_token=API_TOKEN, url="http://abc-123.xyz/radarr"
+            api_token=API_TOKEN, url="http://abc-123.xyz:7878/radarr"
         ),
     )
     assert client._host.url == "http://abc-123.xyz:7878/radarr"
