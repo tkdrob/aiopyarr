94f115cedc17d418cb22b05bccba999b30a5cb8b
Robert Hillis <tkdrob4390@yahoo.com>
2022-06-01 18:04:59 -0400

    Clean up RadarrCalendar model (#46)

--- aiopyarr/models/radarr.py
+++ aiopyarr/models/radarr.py
@@ -288,13 +288,8 @@ class RadarrImportListMovie(_RadarrCommon3):
 
 
 @dataclass(init=False)
-class RadarrCalendarItem(RadarrMovie, _RadarrCommon2):
+class RadarrCalendarItem(RadarrMovie):
     """Radarr calendar attributes."""
-
-    digitalRelease: datetime
-    minimumAvailability: str
-    qualityProfileId: int
-    secondaryYearSourceId: int
 
 
 @dataclass(init=False)
